509fff989525fbca9f31a1b70b480e2beae8ffaa
QuickWrite <git@quickwrite.net>
2026-04-05 20:14:39 +0200

    Implement a simple echo server

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	data := r.PathValue("data")
+
+	w.Write([]byte(data))
+	log.Printf("Received the data: \"%s\"", data)
 }
 
+func main() {
+	http.HandleFunc("/{data}", viewHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
