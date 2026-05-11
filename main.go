814aa49b386ffa79f80aae3437cd9fbd8fc5e51b
QuickWrite <git@quickwrite.net>
2026-05-11 19:08:02 +0200

    Create a better CLI experience.
    The previous CLI experience was rocky at best. Now there is also a help
    message and the concept of flags that allow for extra information to be
    provided.

--- main.go
+++ main.go
@@ -4,15 +4,89 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/QuickWrite/Anansi/markov"
 )
 
+// Converts the provided arguments into two different buckets
+// 1. A list of positional arguments
+// 2. A map of flags with their keys and values.
+//
+// A flag starts with one or two `-` and can contain an argument
+// using an = sign. E. g. --flag=argument.
+// If there is no provided argument, then the map will contain the
+// flag name as an argument and the value as an empty string.
+func parseArgs(v []string) ([]string, map[string]string) {
+	arguments := []string{}
+	flags := map[string]string{}
+
+	for i := range v {
+		if v[i][0] == '-' {
+			flag := v[i][1:]
+
+			if flag[0] == '-' {
+				flag = flag[1:]
+			}
+
+			parts := strings.SplitN(flag, "=", 1)
+
+			if len(parts) > 1 {
+				flags[parts[0]] = parts[1]
+			} else {
+				flags[parts[0]] = ""
+			}
+
+			continue
+		}
+
+		arguments = append(arguments, v[i])
+	}
+
+	return arguments, flags
+}
+
+func contains[T comparable, V any](m map[T]V, k T) bool {
+	_, ok := m[k]
+
+	return ok
+}
+
+func printHelp(path string) {
+	fmt.Printf(`
+Usage:  %s <path> [options]
+
+A mischievous web application to generate an endless stream of senseless text.
+
+  <path>                Path to the text file that will be parsed into a
+                        Markov chain.
+
+Options:
+  -h, --help            Show this help message and exit.
+
+  -l, --limit=<N>       Maximum number of tokens to generate.
+                        If omitted, the default limit is <1000>.
+
+Examples:
+  %s ./books/ulysses.txt                # uses default limit of 1000
+  %s ./data/quotes.txt -l=500           # generate up to 500 tokens
+  %s ./stories.txt --limit=2000         # generate up to 2000 tokens
+  %s ./novel.txt -h                     # display this help screen
+`, path, path, path, path, path)
+}
+
 func main() {
-	args := os.Args[1:]
+	args, flags := parseArgs(os.Args[1:])
+
+	if contains(flags, "help") || contains(flags, "h") {
+		printHelp(os.Args[0])
+
+		os.Exit(0)
+	}
 
 	if len(args) != 1 {
-		fmt.Printf("The program needs the path to the file.")
+		fmt.Printf("The program needs the path to the file.\nTo find out more please write %s --help\n", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -22,9 +96,32 @@ func main() {
 		os.Exit(2)
 	}
 
-	chain = markov.BuildMarkovChain(markov.Tokenize(string(file)))
+	var limit uint = 1000
+	if contains(flags, "l") {
+		l, err := strconv.Atoi(flags["l"])
+
+		if err != nil || l <= 0 {
+			fmt.Printf("The value for -l has to be a positive integer >0 and cannot be %s\n", flags["l"])
+			os.Exit(1)
+		}
+
+		limit = uint(l)
+	}
+
+	if contains(flags, "limit") {
+		l, err := strconv.Atoi(flags["limit"])
+
+		if err != nil || l <= 0 {
+			fmt.Printf("The value for --limit has to be a positive integer >0 and cannot be %s\n", flags["limit"])
+			os.Exit(1)
+		}
+
+		limit = uint(l)
+	}
+
+	chain := markov.BuildMarkovChain(markov.Tokenize(string(file)))
 
 	log.Print("Running Anansi")
 
-	runServer(chain, 1000)
+	runServer(chain, limit)
 }
